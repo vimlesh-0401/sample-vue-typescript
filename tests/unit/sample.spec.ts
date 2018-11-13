e09ddb7876d4c4dfa398e1b80411f93ac71a2e0c
Vimlesh Patel <vimlesh@jombay.com>
2018-11-13 16:18:46 +0530

    truthy test

---
+++ tests/unit/sample.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import { SampleComponent } from '@/app/sample/components';
+
+describe('SampleComponent.vue', () => {
+  it('truthy test', () => {
+    expect(true).eq(true);
+  });
+});

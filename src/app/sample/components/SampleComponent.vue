f4407668834ea82b087fc58cb181a208298dd5c2
Vimlesh Patel <vimlesh@jombay.com>
2018-11-14 23:54:29 +0530

    slider bootstrap

--- src/app/sample/components/SampleComponent.vue
+++ src/app/sample/components/SampleComponent.vue
@@ -9,6 +9,24 @@
       <b-button href="/user-login" variant="primary">Second Module</b-button>
     </b-card>
     <b-card tag="article" style="max-width: 100%" class="mb-2">
+      <b-carousel id="carousel1"
+        background="#ababab"
+        :interval="0"
+        v-model="slide"
+        controls
+        @sliding-start="onSlideStart"
+        @sliding-end="onSlideEnd"
+      >
+        <b-carousel-slide v-for="country in sliderList"
+          :key="country.name"
+          :caption="country.name"
+          img-blank :img-alt="country.name"
+        >
+          <b-table stacked :items="[country.info]"></b-table>
+        </b-carousel-slide>
+      </b-carousel>
+    </b-card>
+    <b-card tag="article" style="max-width: 100%" class="mb-2">
       <b-table stacked :items="list" caption-top>
         <template slot="table-caption">
           Countries list
@@ -32,8 +50,25 @@
     @State('sample') private sample!: SampleState;
     @Getter('message', {namespace}) private message!: string;
 
+    private data() {
+      return {
+        slide: 0,
+      };
+    }
     private mounted() {
       this.sampleData();
     }
+    onSlideStart (slide: any) {
+      console.log('Slide Start: '+slide);
+    }
+    onSlideEnd (slide: any) {
+      console.log('Slide End: '+slide);
+    }
   }
 </script>
+
+<style lang="scss">
+  caption {
+    text-align: center !important;
+  }
+</style>
